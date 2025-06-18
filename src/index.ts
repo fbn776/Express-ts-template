e4afeb2537fae5f026c3d1c030d6440ba8bf33af
fbn776 <fbn776@gmail.com>
2025-06-18 14:40:55 +0530

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import Logger from "./lib/Logger";
+import Logger from "./lib/logger";
 
 require('dotenv').config();
 
@@ -15,6 +15,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/is-alive', (req, res) => {
+    res.status(200).json({
+        status: 'ok',
+        message: 'Server is alive'
+    });
+});
 
 
 
